817904143496f11480d6d95ccca56e5a2ffa1770
Ahmed <ahmedfelah@gmail.com>
2019-06-08 11:32:30 +0300

    change title

--- resources/views/components/layouts/layout.blade.php
+++ resources/views/components/layouts/layout.blade.php
@@ -6,7 +6,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>المرجع العقائدي</title>
+    <title>لوحة تحكم المدونة</title>
 
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
